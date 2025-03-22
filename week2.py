c591926a46b6728fe59a508d7d8fbc0636979f71
Michael-kariuki <kariukimichaelw@gmail.com>
2025-03-22 16:45:10 +0300

    first commit

---
+++ week2.py
@@ -0,0 +1,24 @@
+#Question 1 & 2
+my_list = [10,20,30,40]
+print(my_list)
+
+#Question 3
+my_list.insert(1, 15)
+print(my_list)
+
+#Question 4
+new_num = [50, 60, 70]
+my_list.extend(new_num)
+print(my_list)
+
+#Question 5
+my_list.remove(70)
+print(my_list)
+
+#Question 6
+my_list.sort()
+print(my_list)
+
+#Question 7
+index = my_list.index(30)
+print(index)
